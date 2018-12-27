4b174e60b5de1f75e9918dd911126dab2f0adb87
abulrahmangamer <46195249+abulrahmangamer@users.noreply.github.com>
2018-12-27 18:48:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,97 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-console.log("Scrpit By Dream");
+const prefix = '4';
+
+client.on('message', message => {
+    if(message.content === 'حيدر'){
+        message.channel.send('يوسف')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === 'يوسف'){
+        message.channel.send('الو')
+    }
+});
 
 
-client.on("ready", () => {
-let channel =     client.channels.get("527850226422251523")
-setInterval(function() {
-channel.send(`abulrahman_gamer`);
-}, 30)
-})
+
+client.on('message', message => {
+    if(message.content === 'الو'){
+        message.channel.send('هلا')
+    }
+});
+
+
+
+client.on('message', message => {
+    if(message.content === 'هلا'){
+        message.channel.send('كيفك')
+    }
+});
+
+
+
+
+client.on('message', message => {
+    if(message.content === 'كيفك'){
+        message.channel.send('تمام')
+    }
+});
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content === 'تمام'){
+        message.channel.send('#daily')
+    }
+});
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if(message.content === '#daily'){
+        message.channel.send('حيدر')
+    }
+});
+
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);
